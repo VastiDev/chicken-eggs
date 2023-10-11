eeca19ae856e7aff1887132cf77c96441cdd868e
VastiDev <vastiane1@gmail.com>
2023-10-11 07:36:10 -0300

    start projet

---
+++ src/main/java/com/wakanda/chickeneggs/ChickenEggsApplication.java
@@ -0,0 +1,13 @@
+package com.wakanda.chickeneggs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChickenEggsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChickenEggsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/wakanda/chickeneggs/ChickenEggsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wakanda.chickeneggs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChickenEggsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
