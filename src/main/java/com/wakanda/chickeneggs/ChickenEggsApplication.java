e9341cf3eb7d8ab004367a6bbc0d9d0c8c6da0be
VastiDev <vastiane1@gmail.com>
2023-10-11 17:06:10 -0300

    Implement test API

--- src/main/java/com/wakanda/chickeneggs/ChickenEggsApplication.java
+++ src/main/java/com/wakanda/chickeneggs/ChickenEggsApplication.java
@@ -2,9 +2,19 @@ package com.wakanda.chickeneggs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("/")
 public class ChickenEggsApplication {
+
+	@GetMapping
+	public String getHomeTeste(){
+		return "Chicken Eggs - API Home";
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ChickenEggsApplication.class, args);
