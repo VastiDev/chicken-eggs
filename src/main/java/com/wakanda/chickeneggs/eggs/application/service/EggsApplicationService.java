f08f76c955931689422c336876f275a6b46eb2df
VastiDev <vastiane1@gmail.com>
2023-10-14 18:17:36 -0300

    implements service flow

--- src/main/java/com/wakanda/chickeneggs/eggs/application/service/EggsApplicationService.java
+++ src/main/java/com/wakanda/chickeneggs/eggs/application/service/EggsApplicationService.java
@@ -2,13 +2,18 @@ package com.wakanda.chickeneggs.eggs.application.service;
 
 import com.wakanda.chickeneggs.eggs.application.api.EggsRequest;
 import com.wakanda.chickeneggs.eggs.application.api.EggsResponse;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 @Service
+@Log4j2
 public class EggsApplicationService implements EggsService {
     @Override
     public EggsResponse createEggs(UUID idChicken, EggsRequest eggsRequest) {
+        log.info("[start] EggsApplicationService - createEggs");
+        log.info("[idChicken] {}", idChicken);
+        log.info("[finish] EggsApplicationService - createEggs");
         return null;
     }
 }
