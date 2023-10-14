f03066394f67ffb97cca9541dc59e90aeab0bc3e
VastiDev <vastiane1@gmail.com>
2023-10-14 19:49:01 -0300

    finish list Eggs perChicken

--- src/main/java/com/wakanda/chickeneggs/eggs/application/api/EggsChickenListResponse.java
+++ src/main/java/com/wakanda/chickeneggs/eggs/application/api/EggsChickenListResponse.java
@@ -1,11 +1,34 @@
 package com.wakanda.chickeneggs.eggs.application.api;
 
 import com.wakanda.chickeneggs.eggs.domain.Eggs;
+import lombok.Value;
 
+import javax.validation.constraints.NotNull;
+import java.time.LocalDate;
 import java.util.List;
+import java.util.UUID;
+import java.util.stream.Collectors;
 
+@Value
 public class EggsChickenListResponse {
+
+    @NotNull
+    private UUID idEggs;
+    @NotNull
+    private Integer eggsQuantity;
+    @NotNull
+    private LocalDate hourDateRegistration;
+
+
     public static List<EggsChickenListResponse> convert(List<Eggs> eggsPerChicken) {
-        return null;
+        return eggsPerChicken.stream()
+                .map(EggsChickenListResponse::new)
+                .collect(Collectors.toList());
+    }
+
+    public EggsChickenListResponse(Eggs eggs) {
+        this.idEggs = eggs.getIdEggs();
+        this.eggsQuantity = eggs.getEggsQuantity();
+        this.hourDateRegistration = eggs.getHourDateRegistration();
     }
 }
