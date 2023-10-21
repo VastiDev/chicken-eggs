bdd80fe3003f11c148ce004e552e1cbf2fbfbbfd
VastiDev <vastiane1@gmail.com>
2023-10-20 22:33:43 -0300

    adds localDate in totalRecords Response

--- src/main/java/com/wakanda/chickeneggs/eggsRecord/application/api/ListTotalEggsRecords.java
+++ src/main/java/com/wakanda/chickeneggs/eggsRecord/application/api/ListTotalEggsRecords.java
@@ -3,15 +3,18 @@ package com.wakanda.chickeneggs.eggsRecord.application.api;
 import com.wakanda.chickeneggs.eggsRecord.domain.EggsRecord;
 import lombok.Value;
 
+import java.time.LocalDate;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Value
 public class ListTotalEggsRecords {
     private Integer eggsQuantity;
+    private LocalDate hourDateRegistration;
 
     public ListTotalEggsRecords(EggsRecord eggsRecord) {
         this.eggsQuantity = eggsRecord.getEggsQuantity();
+        this.hourDateRegistration = eggsRecord.getHourDateRegistration();
     }
 
     public static List<ListTotalEggsRecords> convert(List<EggsRecord> totalRecords) {
