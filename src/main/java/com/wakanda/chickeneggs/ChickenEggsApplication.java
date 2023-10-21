5555b5d79fc8022e8b07f41312a08096412be124
VastiDev <vastiane1@gmail.com>
2023-10-21 19:27:52 -0300

    implements swagger, application run but test on server failed

--- src/main/java/com/wakanda/chickeneggs/ChickenEggsApplication.java
+++ src/main/java/com/wakanda/chickeneggs/ChickenEggsApplication.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 @RestController
 @RequestMapping("/")
 public class ChickenEggsApplication {
